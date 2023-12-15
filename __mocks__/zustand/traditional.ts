7ae9929c7b52a6655fb3cbc9c0a94997c9e9e6b8
鸡哥 <114254448+6pjj6@users.noreply.github.com>
2023-12-16 01:21:55 +0800

    Update traditional.ts

--- __mocks__/zustand/traditional.ts
+++ __mocks__/zustand/traditional.ts
@@ -5,6 +5,7 @@ import { createWithEqualityFn as actualCreate } from 'zustand/traditional';
 // a variable to hold reset functions for all stores declared in the app
 const storeResetFns = new Set<() => void>();
 
+
 // when creating a store, we get its initial state, create a reset function and add it in the set
 const createImpl = (createState: any) => {
   const store = actualCreate(createState, Object.is);
